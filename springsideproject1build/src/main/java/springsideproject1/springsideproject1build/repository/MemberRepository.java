e09acc90a04aa8b3ac93397e0d8ba5c79c5d296c
JunHyeok <akdnjs0308@gmail.com>
2024-04-30 19:50:38 +0900

    24.04.30 side project1 build

---
+++ springsideproject1build/src/main/java/springsideproject1/springsideproject1build/repository/MemberRepository.java
@@ -0,0 +1,29 @@
+package springsideproject1.springsideproject1build.repository;
+
+import springsideproject1.springsideproject1build.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    /**
+     * SELECT Data
+     */
+    List<Member> findAllMembers();
+
+    Optional<Member> findMemberByIdentifier(Long identifier);
+
+    Optional<Member> findMemberByID(String id);
+
+    List<Member> findMemberByName(String name);
+
+    /**
+     * INSERT Data
+     */
+    void saveMember(Member member);
+
+    /**
+     * REMOVE Data
+     */
+    void removeMemberByID(String id);
+}
