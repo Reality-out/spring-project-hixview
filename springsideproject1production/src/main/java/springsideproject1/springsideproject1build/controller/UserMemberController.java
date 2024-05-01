e50383dbf4ac46a5f268646bd204ec21f3102cba
JunHyeok <akdnjs0308@gmail.com>
2024-05-02 00:18:22 +0900

    24.05.02 Side Project1

---
+++ springsideproject1production/src/main/java/springsideproject1/springsideproject1build/controller/UserMemberController.java
@@ -0,0 +1,32 @@
+package springsideproject1.springsideproject1build.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springsideproject1.springsideproject1build.domain.Member;
+import springsideproject1.springsideproject1build.domain.MembershipForm;
+import springsideproject1.springsideproject1build.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+public class UserMemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/membership")
+    public String createMembershipForm() {
+        return "user/createMembershipForm";
+    }
+
+    @PostMapping("/membership")
+    public String submitMembershipForm(MembershipForm form) {
+        Member member = new Member();
+        member.setId(form.getId());
+        member.setPassword(form.getPassword());
+        member.setName(form.getName());
+
+        memberService.joinMember(member);
+
+        return "user/finishMembership";
+    }
+}
