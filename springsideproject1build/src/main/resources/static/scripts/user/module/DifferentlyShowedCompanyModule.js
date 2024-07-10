e6b4d92b8576e048fa6f7a4d837dcec24b52909c
JunHyeok <akdnjs0308@gmail.com>
2024-07-10 18:21:00 +0900

    24.07.10 make the visual reaction of the company page better

--- springsideproject1build/src/main/resources/static/scripts/user/module/DifferentlyShowedCompanyModule.js
+++ springsideproject1build/src/main/resources/static/scripts/user/module/DifferentlyShowedCompanyModule.js
@@ -3,20 +3,36 @@
         let bottomComponent = document.getElementById('bottomComponent');
         let bottomLeftComponent = document.getElementById('bottomLeftComponent');
         let bottomRightComponent = document.getElementById('bottomRightComponent');
+        let bottomLeftAllLineComponents = document.querySelector('#bottomLeftComponent > nl');
+        let bottomLeftOneLineComponents = document.querySelectorAll('#bottomLeftComponent > nl > nl');
 
         let viewportWidth = window.innerWidth;
 
+        bottomComponent.classList.remove('long', 'middle', 'short');
+        bottomLeftComponent.classList.remove('longMiddle', 'short');
+        bottomRightComponent.classList.remove('common');
+        bottomLeftAllLineComponents.classList.remove('longMiddle', 'short');
+        bottomLeftOneLineComponents.forEach(component => component.classList.remove('longMiddle', 'short'));
+
         if (viewportWidth >= 1600) {
-            bottomComponent.classList.remove('middle');
             bottomComponent.classList.add('long');
-            bottomLeftComponent.classList.add('common');
-            bottomRightComponent.classList.add('common');
-        } else {
-            bottomComponent.classList.remove('long');
+            bottomLeftComponent.classList.add('longMiddle');
+            bottomLeftAllLineComponents.classList.add('longMiddle');
+            bottomLeftOneLineComponents.forEach(component => component.classList.add('longMiddle'));
+
+        } else if (viewportWidth >= 800) {
             bottomComponent.classList.add('middle');
-            bottomLeftComponent.classList.add('common');
-            bottomRightComponent.classList.add('common');
+            bottomLeftComponent.classList.add('longMiddle');
+            bottomLeftAllLineComponents.classList.add('longMiddle');
+            bottomLeftOneLineComponents.forEach(component => component.classList.add('longMiddle'));
+        } else {
+            bottomComponent.classList.add('short');
+            bottomLeftComponent.classList.add('short');
+            bottomLeftAllLineComponents.classList.add('short');
+            bottomLeftOneLineComponents.forEach(component => component.classList.add('short'));
         }
+
+        bottomRightComponent.classList.add('common');
     }
 
     window.adjustGridLayout = adjustGridLayout;
