a1242d4cb56876bc81b4f6a5e0a6dcd4bb1b2f05
JunHyeok <akdnjs0308@gmail.com>
2024-07-15 13:31:54 +0900

    trying to apply the Git Flow strategy ver5

---
+++ build/springsideproject1/src/main/java/springsideproject1/springsideproject1build/repository/MemberRepository.java
@@ -0,0 +1,29 @@
+package springsideproject1.springsideproject1build.repository;
+
+import springsideproject1.springsideproject1build.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    /**
+     * SELECT Member
+     */
+    List<Member> findAllMembers();
+
+    Optional<Member> findMemberByIdentifier(Long identifier);
+
+    Optional<Member> findMemberByID(String id);
+
+    List<Member> findMemberByName(String name);
+
+    /**
+     * INSERT Member
+     */
+    Long saveMember(Member member);
+
+    /**
+     * REMOVE Member
+     */
+    void removeMemberByID(String id);
+}
