79d26f9b98b57c53fc025e7dbfecb79bcd5ca549
JunHyeok <akdnjs0308@gmail.com>
2024-07-25 23:55:00 +0900

    24.07.25 change the path of the layout to separate that for the user with that for the manager

--- springsideproject1/src/main/java/springsideproject1/springsideproject1build/config/FolderConfig.java
+++ springsideproject1/src/main/java/springsideproject1/springsideproject1build/config/FolderConfig.java
@@ -1,5 +1,5 @@
 package springsideproject1.springsideproject1build.config;
 
 public class FolderConfig {
-    public static String USER_BASIC_LAYOUT_PATH = "/layout/userBasicLayout";
+    public static String USER_BASIC_LAYOUT_PATH = "/layout/user/basicLayout";
 }
