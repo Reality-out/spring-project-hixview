c8cd0ff5f2f96302ab26f25d1d50906850aba094
JunHyeok <akdnjs0308@gmail.com>
2024-07-27 00:59:18 +0900

    24.07.26 set the static attributes for the HTML tags that only have Thymeleaf attributes

--- springsideproject1/src/main/java/springsideproject1/springsideproject1build/config/ViewNameConfig.java
+++ springsideproject1/src/main/java/springsideproject1/springsideproject1build/config/ViewNameConfig.java
@@ -1,8 +1,14 @@
 package springsideproject1.springsideproject1build.config;
 
 public class ViewNameConfig {
-    public static String MANAGER_REMOVE = "manager/remove/";
-
+    /**
+     * User
+     */
     public static String USER_CREATE_MEMBERSHIP = "user/membership/";
+
+    /**
+     * Manager
+     */
     public static String MANAGER_ADD_COMPANY_ARTICLE = "manager/add/article/company/";
+    public static String MANAGER_REMOVE = "manager/remove/";
 }
