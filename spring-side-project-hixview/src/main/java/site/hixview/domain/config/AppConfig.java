0b87a504eba1a39d7da6e91d1622bd7d6855a631
JunHyeok <akdnjs0308@gmail.com>
2024-09-23 19:30:37 +0900

    24.09.23 make the logic of registering the message resources more clearly

--- spring-side-project-hixview/src/main/java/site/hixview/domain/config/AppConfig.java
+++ spring-side-project-hixview/src/main/java/site/hixview/domain/config/AppConfig.java
@@ -4,17 +4,37 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-        source.setBasenames("message.error", "message.manager", "message.others", "message.user");
-        source.setDefaultEncoding(StandardCharsets.UTF_8.toString());
-        return source;
+    public MessageSource messageSource() throws IOException {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames(getMessageResourceBaseNames());
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.toString());
+        return messageSource;
+    }
+
+    private String[] getMessageResourceBaseNames() throws IOException {
+        List<String> baseNames = new ArrayList<>();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath:message/*.properties");
+
+        for (Resource resource : resources) {
+            String filename = resource.getFilename();
+            if (filename != null) {
+                baseNames.add("message/" + filename.substring(0, filename.lastIndexOf('.')));
+            }
+        }
+
+        return baseNames.toArray(new String[0]);
     }
 }
