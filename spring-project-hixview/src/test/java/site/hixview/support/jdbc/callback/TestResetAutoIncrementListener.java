03dd4334b0167b747415562f785b684ff3e241b2
JunHyeok <akdnjs0308@gmail.com>
2024-11-29 14:46:47 +0900

    24.11.29 bind some methods in the ObjectTestUtils and correct the class loader logic

--- spring-project-hixview/src/test/java/site/hixview/support/jdbc/callback/TestResetAutoIncrementListener.java
+++ spring-project-hixview/src/test/java/site/hixview/support/jdbc/callback/TestResetAutoIncrementListener.java
@@ -1,39 +1,16 @@
 package site.hixview.support.jdbc.callback;
 
 import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
-import org.springframework.util.ClassUtils;
-import site.hixview.aggregate.error.NotExpectedClassArgumentException;
 import site.hixview.support.jpa.util.ObjectTestUtils;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import static site.hixview.aggregate.vo.ExceptionMessage.NOT_ENTITY_CLASS;
-import static site.hixview.aggregate.vo.Reference.JPA_ENTITY_REFERENCE;
 
 public class TestResetAutoIncrementListener implements TestExecutionListener, ObjectTestUtils {
 
     private static final Logger log = LoggerFactory.getLogger(TestResetAutoIncrementListener.class);
-
-    @Override
-    public void beforeTestClass(@NonNull TestContext testContext) {
-        resetAutoIncrement(testContext);
-    }
 
     @Override
     public void afterTestClass(@NonNull TestContext testContext) {
@@ -42,40 +19,10 @@ public class TestResetAutoIncrementListener implements TestExecutionListener, Ob
 
     private void resetAutoIncrement(TestContext testContext) {
         JdbcTemplate jdbcTemplate = testContext.getApplicationContext().getBean(JdbcTemplate.class);
-        for (String schemaName : getGeneratedIdClassList().stream().map(this::getTestSchemaNameFromEntity).toList()) {
+        for (String schemaName : ObjectTestUtils.getGeneratedIdClassList().stream().map(ObjectTestUtils::getTestSchemaNameFromEntity).toList()) {
             log.info(schemaName);
             jdbcTemplate.execute("TRUNCATE TABLE " + schemaName);
             jdbcTemplate.execute("ALTER TABLE " + schemaName + " AUTO_INCREMENT = 1");
         }
-    }
-
-    private List<Class<?>> getGeneratedIdClassList() {
-        ArrayList<Class<?>> generatedIdClassList = new ArrayList<>();
-        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
-        ClassLoader classLoader = this.getClass().getClassLoader();
-        scanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
-        for (BeanDefinition beanDefinition : scanner.findCandidateComponents(JPA_ENTITY_REFERENCE)) {
-            Class<?> clazz;
-            try {
-                clazz = ClassUtils.forName(Objects.requireNonNull(beanDefinition.getBeanClassName()), classLoader);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            for (Field field : clazz.getDeclaredFields()) {
-                if (field.isAnnotationPresent(Id.class) && field.isAnnotationPresent(GeneratedValue.class)) {
-                    generatedIdClassList.add(clazz);
-                    break;
-                }
-            }
-        }
-        return generatedIdClassList;
-    }
-
-    private String getTestSchemaNameFromEntity(Class<?> clazz) {
-        Table tableAnnotation = AnnotationUtils.findAnnotation(clazz, Table.class);
-        if (tableAnnotation != null) {
-            return TEST_TABLE_PREFIX + tableAnnotation.name();
-        }
-        throw new NotExpectedClassArgumentException(NOT_ENTITY_CLASS + clazz.getSimpleName());
     }
 }
--- spring-project-hixview/src/test/java/site/hixview/support/jpa/util/ObjectTestUtils.java
+++ spring-project-hixview/src/test/java/site/hixview/support/jpa/util/ObjectTestUtils.java
@@ -1,8 +1,60 @@
 package site.hixview.support.jpa.util;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.ClassUtils;
+import site.hixview.aggregate.error.NotExpectedClassArgumentException;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static site.hixview.aggregate.vo.ExceptionMessage.NOT_ENTITY_CLASS;
+import static site.hixview.aggregate.vo.Reference.JPA_ENTITY_REFERENCE;
+
 public interface ObjectTestUtils {
     /**
      * Constant
      */
     String TEST_TABLE_PREFIX = "test_";
+
+    /**
+     * Method
+     */
+    static List<Class<?>> getGeneratedIdClassList() {
+        ArrayList<Class<?>> generatedIdClassList = new ArrayList<>();
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        scanner.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
+        for (BeanDefinition beanDefinition : scanner.findCandidateComponents(JPA_ENTITY_REFERENCE)) {
+            Class<?> clazz;
+            try {
+                clazz = ClassUtils.forName(Objects.requireNonNull(beanDefinition.getBeanClassName()), classLoader);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Id.class) && field.isAnnotationPresent(GeneratedValue.class)) {
+                    generatedIdClassList.add(clazz);
+                    break;
+                }
+            }
+        }
+        return generatedIdClassList;
+    }
+
+    static String getTestSchemaNameFromEntity(Class<?> clazz) {
+        Table tableAnnotation = AnnotationUtils.findAnnotation(clazz, Table.class);
+        if (tableAnnotation != null) {
+            return TEST_TABLE_PREFIX + tableAnnotation.name();
+        }
+        throw new NotExpectedClassArgumentException(NOT_ENTITY_CLASS + clazz.getSimpleName());
+    }
 }
